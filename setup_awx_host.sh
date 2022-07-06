94ea09dccb9fa791bd7e7cea0f8ded88b260a74a
Jeremy Walker <jwalker@lusd.net>
2022-07-06 10:02:07 -0700

    first commit

---
+++ setup_awx_host.sh
@@ -0,0 +1,25 @@
+sudo
+sudo dnf -y update
+sudo curl https://download.docker.com/linux/centos/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo
+sudo sed -i -e "s/enabled=1/enabled=0/g" /etc/yum.repos.d/docker-ce.repo
+sudo dnf --enablerepo=docker-ce-stable -y install docker-ce
+sudo dnf -y groupinstall "Development Tools"
+sudo dnf -y install openssh-server git epel-release openssl ansible python3.9
+
+# install docker-compose
+sudo DOCKER_CONFIG=/usr/local/lib/docker && \
+    sudo mkdir -p $DOCKER_CONFIG/cli-plugins && \
+    sudo curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose && \
+    sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+# docker-compose symlink so sudo can use it too
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# upgrade pip3 and install docker-compose module
+sudo pip3 install --upgrade pip
+sudo pip3 install docker-compose
+
+# Config SSHD
+# Defining the Port 22 for service and point to banner
+sudo sed -ie 's/#Port 22/Port 22/g' /etc/ssh/sshd_config \
+  && sed -ie 's|#Banner none|Banner /etc/ssh/sshd-banner|g' /etc/ssh/sshd_config
